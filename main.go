a6c3be2d2cc679fbfc2c58384729f87b97e6f1bb
Harry Thompson <harrythompson3765@gmail.com>
2026-04-24 18:48:10 +0100

    temp main

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"harry/session/src/config"
+	"harry/session/src/session"
+)
+
 func main() {
+	sessions, err := session.FindSessions(config.Config{
+		SearchPaths: []string{
+			"~/dev/*",
+			"~/work/*",
+		},
+		IncludePaths: []string{
+			"~/env",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, session := range sessions {
+		fmt.Printf("%v\n", session)
+	}
 }
