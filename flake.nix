bf49d12005397125b1a462d41c8fb04812083662
Harry Thompson <harrythompson3765@gmail.com>
2026-05-02 20:01:16 +0100

    version

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
         perSystem = { pkgs, ... }: {
             packages.default = pkgs.buildGoModule {
                 pname = "session";
-                version = "1.0.1";
+                version = "1.0.2";
 
                 src = ./.;
 
